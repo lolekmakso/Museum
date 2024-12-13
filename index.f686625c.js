433a39dc4ac8d2057bf0e0595c2d7045adf1d86b
Maksym <makstarasovv1@gmail.com>
2024-12-13 13:22:18 +0200

    make build

---
+++ index.f686625c.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{loop:!0,spaceBetween:16,pagination:{el:".swiper-pagination"},breakpoints:{0:{slidesPerView:1},768:{slidesPerView:2},1040:{slidesPerView:3}}});
+//# sourceMappingURL=index.f686625c.js.map
